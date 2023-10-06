05c5c39cdd08ca278108215a55860501e6a3edeb
Erwin Hörnell <erwin.hornell@learnet.se>
2023-10-06 13:06:57 +0200

    chest

--- 2d platformer group/scripts/player.gd
+++ 2d platformer group/scripts/player.gd
@@ -136,10 +136,10 @@ func _physics_process(delta):
 	update_shape()
 	move_and_slide()
 
-#	for i in get_slide_collision_count():
-#		var collision = get_slide_collision(i)
-#		if collision.get_collider().get_parent() is Trap:
-#			position = start_pos
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision.get_collider().get_parent() is Trap:
+			position = start_pos
 
 
 func update_animations():
